1be8bca64124cfedc9dd55319567dfbcab299e33
Agustin Recalde <arecalde.zteam@zuora.com>
2018-09-03 17:57:20 -0700

    Rearranged the code on the UserData script

--- dependencies.sh
+++ dependencies.sh
@@ -11,21 +11,33 @@ pip install --upgrade pipenv
 # Install Pipenv
 pip install pipenv
 
-# Download the code from GitHub
-git clone git@github.com:Northwood128/ec2-demo-app.git
+# Download the code from GitHub and install dependencies
+git clone git@github.com:Northwood128/ec2-demo-app.git /opt/EC2DemoApp
 
-# Install app
+cd /opt/EC2DemoApp
+# https://pipenv.readthedocs.io/en/latest/advanced/#custom-virtual-environment-location
 export PIPENV_VENV_IN_PROJECT=true
+pipenv install
+
+# Move Nginx configuration
+mv nginx.conf /etc/nginx.conf
+# Move SystemD Unit file
+mv demoapp.service /etc/systemd/system/demoapp.service
+
+# Setup right permissions on the directory
+chmod 710 /opt/EC2DemoApp
 
 # Create a service user to run the app
 adduser demo-user
 
-# Add the Nginx user to the service user, to allow it to access the app
+# Add the Nginx user to the service user's group, to allow it to access the app
 sudo usermod -a -G demo-user nginx
-chmod 710 /opt/EC2DemoApp
-nginx -t
+/usr/sbin/nginx -t
 
+# Start Gunicorn
 systemctl start ec2demoapp
 systemctl enable ec2demoapp
+
+# Start Nginx
 systemctl start nginx
 systemctl enable nginx
