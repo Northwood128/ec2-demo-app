21c6580e61ed6cb8714be92aa0339eaf687b7f0c
Agustin Recalde <arecalde.zteam@zuora.com>
2018-09-03 18:44:04 -0700

    Updated demoapp.service, dependencies.sh, nginx.conf and Pipfile

--- dependencies.sh
+++ dependencies.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # Enable EPEL repo
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum makecache fast
+sudo yum update -y
 
 # Install the necessary packages
-yum install -y git python-pip python36 python36-devel gcc nginx
+yum install -y git python3-pip python36 python36-devel gcc nginx
 
 # Upgrade Pip
-pip install --upgrade pipenv
+pip3 install --upgrade pipenv
 
 # Install Pipenv
-pip install pipenv
+pip3 install pipenv
 
 # Download the code from GitHub and install dependencies
-git clone git@github.com:Northwood128/ec2-demo-app.git /opt/EC2DemoApp
+git clone https://github.com/Northwood128/ec2-demo-app.git /opt/EC2DemoApp
 
 cd /opt/EC2DemoApp
 # https://pipenv.readthedocs.io/en/latest/advanced/#custom-virtual-environment-location
@@ -35,8 +37,8 @@ sudo usermod -a -G demo-user nginx
 /usr/sbin/nginx -t
 
 # Start Gunicorn
-systemctl start ec2demoapp
-systemctl enable ec2demoapp
+systemctl start demoapp
+systemctl enable demoapp
 
 # Start Nginx
 systemctl start nginx
