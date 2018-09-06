3e3bc9635563aad719d6ecb751cdddc671ffbcac
Agustin Recalde <arecalde@edrans.com>
2018-09-06 12:09:05 -0700

    Removed try/except clause used during testing
    Signed-off-by: Agustin Recalde <arecalde@edrans.com>

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ EC2DemoApp = Flask(__name__)
 
 @EC2DemoApp.route("/")
 def hello():
-    try:
-        with urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id') as response:
-        #with urllib.request.urlopen('http://httpbin.org/status/418') as response:
-            instance_id = response.read()
-    except HTTPError as e:
-        instance_id = "Teapot"
+    with urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id') as response:
+        instance_id = response.read()
 
     return render_template('index.html', instance_id=str(instance_id))
 
